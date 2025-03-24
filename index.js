039cff523a9b9aaa566e718f40d81f52e8c15c4e
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2025-03-24 11:44:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Simple function to validate DNI and NIE.
+ *
+ * @param {String} dni - DNI (or NIE) to validate.
+ * @param {boolean} [excludeNIE=false] - Specifies if NIE should be excluded from validations.
+ * @returns {boolean} True if DNI | NIE is valid, otherwise false.
+ */
+function validateDNI(dni, excludeNIE = false) {
+    let string = String(dni).toLocaleUpperCase()
+    if (!excludeNIE) {
+        string = string.replace(/^[X]/, "0").replace(/^[Y]/, "1").replace(/^[Z]/, "2")
+    }
+
+    const letters = "TRWAGMYFPDXBNJZSQVHLCKE"
+    const regex = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i
+    if (!regex.test(string)) return false
+
+    const numbers = Number(string.slice(0, 8))
+    const module = numbers % 23
+    return string[8].toUpperCase() === letters[module]
+}
